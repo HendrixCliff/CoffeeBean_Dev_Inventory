67bd0495c7da78652d11d1ef03d80eca2b8a5ee9
Godwin Chukwuebuka Igwegbe <godwincliff10@email.com>
2025-05-22 04:00:44 +0100

    Flee

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './Dashboard';
-import Login from "./components/LoginComponent"
-import ForgotPassword from "./components/ForgotPasswordComponent"
-import SignUp from "./components/SignUpComponent"
-import  ResetPassword from "./components/ResetPasswordComponent"
-import FilteredItemsList from "./components/FilteredItemsList"
+import Login from "./components/LoginComponent";
+import ForgotPassword from "./components/ForgotPasswordComponent";
+import SignUp from "./components/SignUpComponent";
+import ResetPassword from "./components/ResetPasswordComponent";
+import FilteredItemsList from "./components/FilteredItemsList";
 import UploadReceiptForm from './components/UploadReceiptForm';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/filter" element={<FilteredItemsList />} />
-        <Route path="/upload-receipt" element={<UploadReceiptForm />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/filter" element={<FilteredItemsList />} />
+          <Route path="/upload-receipt" element={<UploadReceiptForm />} />
+        </Routes>
+      </ErrorBoundary>
     </Router>
   );
 }
